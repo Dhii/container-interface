72e5d4f56690babc3fd629ae5962869b8a592d41
Anton Ukhanev <xedin.unknown@gmail.com>
2017-08-28 00:23:05 +0200

    Added `DataKeyAwareInterface`

---
+++ src/DataKeyAwareInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dhii\Data\Container;
+
+/**
+ * Something that can have a data key retrieved from it.
+ *
+ * @since [*next-version*]
+ */
+interface DataKeyAwareInterface
+{
+    /**
+     * Retrieves the key that is associated with this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @return string|Stringable|null The key, if any.
+     */
+    public function getDataKey();
+}
